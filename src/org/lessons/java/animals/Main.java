9ec1aa72b38127883ee25820f7836cf0383f1063
Erika Cetera <ceteraerika@gmail.com>
2025-03-27 11:26:26 +0100

    feat: interfacce nel main

--- src/org/lessons/java/animals/Main.java
+++ src/org/lessons/java/animals/Main.java
@@ -1,44 +1,54 @@
 package org.lessons.java.animals;
 
 public class Main {
-public static void main(String[] args) {
-    
+    public static void main(String[] args) {
 
-    Dog Cane = new Dog("Cane");
-    Eagle Aquila = new Eagle("Aquila");
-    Dolphin Delfino = new Dolphin("Delfino");
-    Sparrow Passero = new Sparrow("Passero");
+        Dog Cane = new Dog("Cane");
+        Eagle Aquila = new Eagle("Aquila");
+        Dolphin Delfino = new Dolphin("Delfino");
+        Sparrow Passero = new Sparrow("Passero");
 
-    
-    System.out.println(Cane);
-    System.out.println(Cane.sleep());
-    Cane.verse();
-    Cane.eat();
+        System.out.println(Cane);
+        System.out.println(Cane.sleep());
+        Cane.verse();
+        Cane.eat();
 
-    System.out.println("-------------");
+        System.out.println("-------------");
 
-    System.out.println(Delfino);
-    System.out.println(Delfino.sleep());
-    Delfino.verse();
-    Delfino.eat();
-    Delfino.swim();
+        System.out.println(Delfino);
+        System.out.println(Delfino.sleep());
+        Delfino.verse();
+        Delfino.eat();
+        Delfino.swim();
 
-    System.out.println("-------------");
+        System.out.println("-------------");
 
-    System.out.println(Aquila);
-    System.out.println(Aquila.sleep());
-    Aquila.verse();
-    Aquila.eat();
-    Aquila.fly();
+        System.out.println(Aquila);
+        System.out.println(Aquila.sleep());
+        Aquila.verse();
+        Aquila.eat();
+        Aquila.fly();
 
+        System.out.println("-------------");
 
-    System.out.println("-------------");
+        System.out.println(Passero);
+        System.out.println(Passero.sleep());
+        Passero.verse();
+        Passero.eat();
+        Passero.fly();
 
-    System.out.println(Passero);
-    System.out.println(Passero.sleep());
-    Passero.verse();
-    Passero.eat();
-    Passero.fly();
+        System.out.println("-------------");
 
-}
+        fly(Aquila);
+        swim(Delfino);
+
+    }
+
+    static void fly(Flyable animal) {
+        animal.fly();
+    }
+
+    static void swim(Swimable animal) {
+        animal.swim();
+    }
 }
